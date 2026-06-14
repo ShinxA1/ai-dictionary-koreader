b130282f88b02a93d9ce2c905936b2933aa66e55
ShinxA1 <127334528+ShinxA1@users.noreply.github.com>
2026-06-13 22:42:14 -0300

    Localize fullname and description in _meta.lua

--- AI_Dictionary.koplugin/_meta.lua
+++ AI_Dictionary.koplugin/_meta.lua
@@ -1,6 +1,8 @@
+local _ = require("l10n/aidictionary_l10n")
+
 return {
     name = "aidictionary",
-    fullname = "AI Dictionary",
-    description = "AI dictionary and explainer",
+    fullname = _("AI Dictionary"),
+    description = _("AI dictionary and explainer"),
     version = 2.0,
 }
